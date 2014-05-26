8ff40ab8ad08cfd97f27a723ff40f7632a92168d
songsong <xiongsongsong@outlook.com>
2014-05-26 22:59:32 +0800

    Initial commit

---
+++ assets/excel.js
@@ -0,0 +1,20 @@
+/**
+ * Created by xiongsongsong on 14-5-25.
+ * 获取Excel中的各种数据
+ */
+
+define(function (require, exports, module) {
+
+    //重置列背景和列高
+    exports.resetColsBackgroundPosition = function () {
+
+    }
+
+    //重置行背景
+    exports.resetRowsBackgroundPosition = function () {
+
+    }
+
+
+
+})
