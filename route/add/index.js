2c5db7de966d8b37486d134751c74dfd64b5cd59
xiongsongsong <xiongsongsong@outlook.com>
2014-07-06 11:21:40 +0800

    删除无用的注释

--- route/add/index.js
+++ route/add/index.js
@@ -77,10 +77,9 @@ add.post('/page', function (req, res) {
         }
 
         //检索并处理为table表达式的行
-        body.content = body.content.split(/\r\n/gm).map(function (line) {
+        body.content = body.content.split(/\r\n/).map(function (line) {
             //在保存前，生成一个ID放置在模板引擎的末尾
             //此处的ID，在data集合中的key为pageId
-            console.log(tableRe.test(line), line)
             if (tableRe.test(line) && !/_id=[a-z0-9]{24}/.test(line)) {
                 return line + ',_id=' + new ObjectID()
             } else {
