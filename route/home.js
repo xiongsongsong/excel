887c4185721f0bc3bbf3576d6d05b3bc81a71042
xiongsongsong <xiongsongsong@outlook.com>
2014-07-06 22:40:55 +0800

    增加moment模块

--- route/home.js
+++ route/home.js
@@ -1,27 +1,30 @@
 var express = require('express')
 var home = express.Router()
 var db = require('db').db
-var template = require('template')
 var ObjectID = require('mongodb').ObjectID
 var xss = require('xss')
+var moment = require('moment')
+moment.lang('zh-cn')
 
 home.get('/', function (req, res) {
     res.header('Cache-Control', 'no-cache')
     var tpl = db.collection('tpl')
     //查询所有最后修改后的文档，倒序排列
     tpl.aggregate([
-        {
-            $sort: {ts: 1}
-        },
+        {$project: { name: 1, url: 1, ts: 1, tplId: 1 }},
+        {$sort: {ts: -1}},
         {
             $group: {
                 _id: "$tplId",
-                last_url: {$last: "$ts"},
-                name: {$last: '$name'}
+                ts: {$first: "$ts"},
+                url: {$first: '$url'},
+                name: {$first: '$name'}
             }
         }
     ], function (err, docs) {
-        res.render('index', {docs: docs})
+        //待解决问题：aggregate不能对返回结果进行再排序？
+        //此处临时解决方案，手动排序下
+        res.render('index', {docs: docs.reverse(), moment: moment})
     })
 
 })
